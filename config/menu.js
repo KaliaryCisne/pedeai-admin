e982266ea7d043eaacd500f5e14dad9d0c4c3807
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2020-09-13 00:04:22 -0300

    first commit

---
+++ config/menu.js
@@ -0,0 +1,58 @@
+const menu = [
+  {
+    "group": "Administração",
+    "items": [
+      {
+        "name": "Usuários",
+        "icon": "group",
+        "subitems": [
+          {
+            "text": "Listar",
+            "path": "user.html",
+          },
+          {
+            "text": "Adicionar",
+            "path": "#",
+          }
+        ]
+      },
+      {
+        "name": "Relatórios",
+        "icon": "print",
+        "subitems": []
+      },
+      {
+        "name": "Estoque",
+        "icon": "table_chart",
+        "subitems": []
+      }
+    ],
+  },
+  {
+    "group": "Restaurante",
+    "items": [],
+  },
+  {
+    "group": "Cardápio",
+    "items": [
+      {
+        "name": "Categorias",
+        "icon": "tag",
+        "subitems": [
+          {
+            "text": "Listar",
+            "path": "#",
+          },
+          {
+            "text": "Adicionar",
+            "path": "#",
+          }
+        ]
+      },
+    ],
+  },
+  {
+    "group": "Financeiro",
+    "items": [],
+  },
+];
